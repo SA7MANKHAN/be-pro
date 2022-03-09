72b03f83fedb9cd8b6c9f731c124b5f756a0b8e6
Salman <salmanwbillo@gmail.com>
2022-03-08 21:42:04 -0800

    Initial Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,68 @@
 //
+$(".nav-toggler").click(function(){
+    $(".nav-items").toggleClass("show");
+});
+
+// Hero Slider
+$('.hero-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    dots: true,
+    items: 1,
+    smartSpeed: 1000,
+    navText: ["<i class='ri-arrow-left-line'></i>", "<i class='ri-arrow-right-line'></i>"],
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     600:{
+    //         items:3
+    //     },
+    //     1000:{
+    //         items:5
+    //     }
+    // }
+})
+
+// Porfolio Slider
+$('.portfolio-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    dots: false,
+    smartSpeed: 1000,
+    margin: 24,
+    navText: ["<i class='ri-arrow-left-line'></i>", "<i class='ri-arrow-right-line'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+})
+
+
+// Reviews Slider
+$('.reviews-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    dots: false,
+    smartSpeed: 1000,
+    margin: 24,
+    navText: ["<i class='ri-arrow-left-line'></i>", "<i class='ri-arrow-right-line'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        992:{
+            items:2
+        }
+    }
+})
